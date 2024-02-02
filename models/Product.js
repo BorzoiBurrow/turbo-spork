5f23cf0c589dafca2dcfec076f15b95740b99f9a
Brad <Bradrockk@gmail.com>
2024-02-02 09:25:33 -0500

    filled out models relating to seeds

--- models/Product.js
+++ models/Product.js
@@ -6,10 +6,27 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+
 Product.init(
   {
-    // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    stock: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    Product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    }
   },
   {
     sequelize,
