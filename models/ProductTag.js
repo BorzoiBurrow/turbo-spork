221f5f9ae26a3524bbef56288190b257f438926a
Brad <Bradrockk@gmail.com>
2024-02-02 08:40:52 -0500

    filled in product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // primary key ID 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // products numeric ID
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+      // name of the product
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
   },
   {
     sequelize,
