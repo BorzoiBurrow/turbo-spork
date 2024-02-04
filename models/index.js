5db61b6ea1b03914a3a71cf009314507b335c4a8
Brad <Bradrockk@gmail.com>
2024-02-04 10:39:45 -0500

    filled in index.js for models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_name',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_name',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_name',
+});
+// expors
 module.exports = {
   Product,
   Category,
